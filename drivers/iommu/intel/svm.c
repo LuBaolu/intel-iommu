6aebfc17770ace02b00262633e04112bda04f076
Lu Baolu <baolu.lu@linux.intel.com>
2022-03-09 11:31:58 +0800

    iommu/vt-d: Set PGSNP bit in pasid table entry for sva binding
    This field make the requests snoop processor caches irrespective of other
    attributes in the request or other fields in paging structure entries
    used to translate the request. The latest VT-d specification states that
    this field is treated as Reserved(0) for implementations not supporting
    Snoop Control (SC=0 in the Extended Capability Register). Hence add a
    check in the code.
    
    Signed-off-by: Lu Baolu <baolu.lu@linux.intel.com>

--- drivers/iommu/intel/svm.c
+++ drivers/iommu/intel/svm.c
@@ -394,6 +394,7 @@ static struct iommu_sva *intel_svm_bind_mm(struct intel_iommu *iommu,
 	sflags = (flags & SVM_FLAG_SUPERVISOR_MODE) ?
 			PASID_FLAG_SUPERVISOR_MODE : 0;
 	sflags |= cpu_feature_enabled(X86_FEATURE_LA57) ? PASID_FLAG_FL5LP : 0;
+	sflags |= PASID_FLAG_PAGE_SNOOP;
 	spin_lock_irqsave(&iommu->lock, iflags);
 	ret = intel_pasid_setup_first_level(iommu, dev, mm->pgd, mm->pasid,
 					    FLPT_DEFAULT_DID, sflags);
