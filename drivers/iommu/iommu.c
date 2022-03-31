3ea6d61ff4ee38eb4db9bb11695028fa71938ad5
Lu Baolu <baolu.lu@linux.intel.com>
2022-03-31 16:35:58 +0800

    iommu: Remove SVM_FLAG_SUPERVISOR_MODE support
    The current kernel DMA with PASID support is based on the SVA with a flag
    SVM_FLAG_SUPERVISOR_MODE. The IOMMU driver binds the kernel memory address
    space to a PASID of the device. The device driver programs the device with
    kernel virtual address (KVA) for DMA access. There have been security and
    functional issues with this approach:
    
    - The lack of IOTLB synchronization upon kernel page table updates.
      (vmalloc, module/BPF loading, CONFIG_DEBUG_PAGEALLOC etc.)
    - Other than slight more protection, using kernel virtual address (KVA)
      has little advantage over physical address. There are also no use
      cases yet where DMA engines need kernel virtual addresses for in-kernel
      DMA.
    
    This removes SVM_FLAG_SUPERVISOR_MODE support from the IOMMU interface.
    The device drivers are suggested to handle kernel DMA with PASID through
    the kernel DMA APIs.
    
    The drvdata parameter in iommu_sva_bind_device() and all callbacks is not
    needed anymore. Cleanup them as well.
    
    Link: https://lore.kernel.org/linux-iommu/20210511194726.GP1002214@nvidia.com/
    Signed-off-by: Jacob Pan <jacob.jun.pan@linux.intel.com>
    Signed-off-by: Lu Baolu <baolu.lu@linux.intel.com>
    Reviewed-by: Jason Gunthorpe <jgg@nvidia.com>

--- drivers/iommu/iommu.c
+++ drivers/iommu/iommu.c
@@ -2768,7 +2768,6 @@ EXPORT_SYMBOL_GPL(iommu_dev_feature_enabled);
  * iommu_sva_bind_device() - Bind a process address space to a device
  * @dev: the device
  * @mm: the mm to bind, caller must hold a reference to it
- * @drvdata: opaque data pointer to pass to bind callback
  *
  * Create a bond between device and address space, allowing the device to access
  * the mm using the returned PASID. If a bond already exists between @device and
@@ -2781,7 +2780,7 @@ EXPORT_SYMBOL_GPL(iommu_dev_feature_enabled);
  * On error, returns an ERR_PTR value.
  */
 struct iommu_sva *
-iommu_sva_bind_device(struct device *dev, struct mm_struct *mm, void *drvdata)
+iommu_sva_bind_device(struct device *dev, struct mm_struct *mm)
 {
 	struct iommu_group *group;
 	struct iommu_sva *handle = ERR_PTR(-EINVAL);
@@ -2806,7 +2805,7 @@ iommu_sva_bind_device(struct device *dev, struct mm_struct *mm, void *drvdata)
 	if (iommu_group_device_count(group) != 1)
 		goto out_unlock;
 
-	handle = ops->sva_bind(dev, mm, drvdata);
+	handle = ops->sva_bind(dev, mm);
 
 out_unlock:
 	mutex_unlock(&group->mutex);
