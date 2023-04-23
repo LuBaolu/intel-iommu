c43078d02d01756a3724f6146ae4919a870bf4a3
Lu Baolu <baolu.lu@linux.intel.com>
2023-04-23 14:48:47 +0800

    iommu: Add unrecoverable fault handling support
    Signed-off-by: Lu Baolu <baolu.lu@linux.intel.com>

--- include/linux/iommu.h
+++ include/linux/iommu.h
@@ -97,6 +97,10 @@ struct iommu_domain {
 	unsigned long pgsize_bitmap;	/* Bitmap of page sizes in use */
 	struct iommu_domain_geometry geometry;
 	struct iommu_dma_cookie *iova_cookie;
+
+	int (*dmaf_handler)(struct iommu_fault *fault, void *data);
+	void *dmaf_data;
+
 	enum iommu_page_response_code (*iopf_handler)(struct iommu_fault *fault,
 						      void *data);
 	void *fault_data;
@@ -115,6 +119,24 @@ struct iommu_domain {
 static inline bool iommu_is_dma_domain(struct iommu_domain *domain)
 {
 	return domain->type & __IOMMU_DOMAIN_DMA_API;
+}
+
+static inline void
+iommu_set_dma_fault_handler(struct iommu_domain *domain,
+			    int (*handler)(struct iommu_fault *fault, void *data),
+			    void *data)
+{
+	domain->dmaf_handler = handler;
+	domain->dmaf_data = data;
+}
+
+static inline void
+iommu_set_iopage_fault_handler(struct iommu_domain *domain,
+			       enum iommu_page_response_code (*handler)(struct iommu_fault *fault, void *data),
+			       void *data)
+{
+	domain->iopf_handler = handler;
+	domain->fault_data = data;
 }
 
 enum iommu_cap {
@@ -522,6 +544,7 @@ extern int iommu_report_device_fault(struct device *dev,
 				     struct iommu_fault_event *evt);
 extern int iommu_page_response(struct device *dev,
 			       struct iommu_page_response *msg);
+int iommu_device_fault_handler(struct iommu_fault *fault, void *cookie);
 
 extern int iommu_group_id(struct iommu_group *group);
 extern struct iommu_domain *iommu_group_default_domain(struct iommu_group *);
@@ -919,6 +942,12 @@ int iommu_report_device_fault(struct device *dev, struct iommu_fault_event *evt)
 
 static inline int iommu_page_response(struct device *dev,
 				      struct iommu_page_response *msg)
+{
+	return -ENODEV;
+}
+
+static inline int iommu_device_fault_handler(struct iommu_fault *fault,
+					     void *cookie)
 {
 	return -ENODEV;
 }
